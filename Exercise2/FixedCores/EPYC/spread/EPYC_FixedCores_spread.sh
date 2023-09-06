6e7b652b39e4fec8b44f24eafe18414aa1924a1c
SilviaImeneo1 <silvia.imeneo@gmail.com>
2023-09-06 17:36:52 +0200

    upload sh files

---
+++ Exercise2/FixedCores/EPYC/spread/EPYC_FixedCores_spread.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#SBATCH --no-requeue
+#SBATCH --job-name="Epyc_FixedCores_spread"
+#SBATCH -n 64
+#SBATCH -N 1
+#SBATCH --get-user-env
+#SBATCH --partition=EPYC
+#SBATCH --exclusive
+#SBATCH --time=02:00:00
+#SBATCH --output="summary.out"
+
+# Load required modules
+module load architecture/AMD
+module load mkl
+module load openBLAS/0.3.23-omp
+
+# Add BLIS library path to LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/u/dssc/gcosta00/assignment/exercise2/blis/lib:$LD_LIBRARY_PATH
+
+# Store the current working directory
+directory=$(pwd)
+
+# Set OpenMP policy, threads, and binding
+policy=spread
+arch=EPYC #architecture
+
+# Function to clean and compile
+clean_and_compile() {
+  cd "$1"
+  make clean loc="$2"
+  make all loc="$2"
+  cd "$2"
+}
+
+# Function to run benchmarks
+run_benchmarks() {
+  for lib in openblas mkl blis; do
+    for prec in float double; do
+      file="${lib}_${prec}.csv"
+      echo "matrix_size,time_mean(s),time_sd,GFLOPS_mean,GFLOPS_sd" > "$file"
+    done
+  done
+
+  for i in {0..18}; do
+    let size=$((2000+1000*$i))
+    for lib in openblas mkl blis; do
+      for prec in float double; do
+        echo -n "${size}," >> "${lib}_${prec}.csv"
+        "./${lib}_${prec}.x" "$size" "$size" "$size"
+      done
+    done
+  done
+}
+
+# Run tasks
+clean_and_compile ../../.. "$directory"
+cd "$directory"
+
+export OMP_PLACES=cores
+export OMP_PROC_BIND="$policy"
+export OMP_NUM_THREADS=64
+
+run_benchmarks
+
+clean_and_compile ../../.. "$directory"
+module purge
+
---
+++ Exercise2/FixedSize/THIN/spread/THIN_fizedSize_spread_mkl.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#SBATCH --no-requeue
+#SBATCH --job-name="THIN_mkl_FixedSize_spread"
+#SBATCH -n 24
+#SBATCH -N 1
+#SBATCH --get-user-env
+#SBATCH --partition=THIN
+#SBATCH --exclusive
+#SBATCH --time=02:00:00
+#SBATCH --output="summary_1.out"
+
+# Load required mo
+dules
+module load architecture/Intel
+module load mkl
+module load openBLAS/0.3.23-omp
+
+# Add BLIS library path to LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/u/dssc/gcosta00/assignment/exercise2/blis/lib:$LD_LIBRARY_PATH
+
+# Store the current working directory
+directory=$(pwd)
+
+# Set OpenMP policy, threads, and binding
+policy=spread
+arch=THIN #architecture
+
+# Function to clean and compile
+clean_and_compile() {
+  cd "$1"
+  make clean loc="$2"
+  make all loc="$2"
+  cd "$2"
+}
+
+# Function to run benchmarks
+run_benchmarks() {
+  for lib in mkl; do
+    for prec in float double; do
+      file="${lib}_${prec}.csv"
+      echo "num_cores,time_mean(s),time_sd,GFLOPS_mean,GFLOPS_sd" > "$file"
+    done
+  done
+
+  for cores in {1..24}; do
+    for lib in mkl; do
+      for prec in float double; do
+        echo -n "${cores}," >> "${lib}_${prec}.csv"
+        export OMP_PLACES=cores
+        export OMP_PROC_BIND="$policy"
+        export OMP_NUM_THREADS="$cores"
+        "./${lib}_${prec}.x" "10000" "10000" "10000"
+      done
+    done
+  done
+}
+
+# Run tasks
+clean_and_compile ../../.. "$directory"
+cd "$directory"
+
+run_benchmarks
+
+clean_and_compile ../../.. "$directory"
+module purge
+
